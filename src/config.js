d34ed54fc70e8b1bdcbb0af29e0ea835c868c010
YUVRAJ005 <abhishek.iche@gmail.com>
2023-09-27 22:24:34 +0530

    added .env file

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const server = "http://localhost:3000/Dashboard";
-
-const auth0domain="dev-ug5wbcmf8y8ide7v.us.auth0.com"
-const auth0clientId="oLS4A8L6C01LmST3hOke9rEvOWCmJfc2"
-
-module.exports = {server,auth0domain,auth0clientId};
--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
-import { server, auth0domain, auth0clientId } from './config'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
-    domain = {auth0domain}
-    clientId = {auth0clientId}
+    domain = {process.env.REACT_APP_auth0domain}
+    clientId = {process.env.REACT_APP_auth0clientId}
     authorizationParams={{
       redirect_uri: window.location.origin + "/Dashboard"
     }}>
